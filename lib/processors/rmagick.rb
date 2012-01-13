c08ee8cb9e034355760bb31c4f9db8236b5b966a
James Cammarata <jimi@sngx.net>
2012-01-12 22:44:10 -0600

    Added ability to read in a blob created by another RMagick image.

--- lib/processors/rmagick.rb
+++ lib/processors/rmagick.rb
@@ -8,4 +8,13 @@ module RMagickProcessor
     cat.write tmp_file.to_s
     return tmp_file
   end
+
+  def image_from_blob(blob)
+    generate_uid
+    tmp_file = Pathname.new(Dir::tmpdir).join("#{@uid}_#{@source.basename}.tif").to_s
+    cat = Magick::Image.from_blob(blob).first
+    cat.crop!(@x, @y, @w, @h) unless [@x, @y, @w, @h].compact == []
+    cat.write tmp_file.to_s
+    return tmp_file
+  end
 end
