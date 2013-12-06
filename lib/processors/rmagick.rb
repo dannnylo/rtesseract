e455704646529970cb5789aa5c053c4f4c86adfb
TJ Moretto <tj.moretto@musclesound.com>
2013-12-06 15:59:19 -0800

    Attempt to skip compression when converting from jpeg to tiff

--- lib/processors/rmagick.rb
+++ lib/processors/rmagick.rb
@@ -5,7 +5,7 @@ module RMagickProcessor
     tmp_file = Tempfile.new(["",".tif"])
     cat = @instance || Magick::Image.read(@source.to_s).first
     cat.crop!(@x, @y, @w, @h) unless [@x, @y, @w, @h].compact == []
-    cat.write tmp_file.path.to_s
+    cat.write tmp_file.path.to_s, "-compress none"
     return tmp_file
   end
 
