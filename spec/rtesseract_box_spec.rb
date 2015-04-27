35b4ae4eca8f23deca4cbbbe3059fc9dfb7e7114
Danilo Jeremias da Silva <danilo.silva@euax.com.br>
2015-04-27 11:49:02 -0300

    Box

--- spec/rtesseract_box_spec.rb
+++ spec/rtesseract_box_spec.rb
@@ -9,14 +9,12 @@ describe "Rtesseract::Box" do
 
 
   it "bounding box" do
-    RTesseract::Box.new(@image_tiff).characters.is_a?(Array).should be_true
+    RTesseract::Box.new(@image_tiff).characters.is_a?(Array).should == true
     RTesseract::Box.new(@image_tiff).characters.should eql([
       {:char=>"4", :x_start=>145, :y_start=>14, :x_end=>159, :y_end=>33},
       {:char=>"3", :x_start=>184, :y_start=>14, :x_end=>196, :y_end=>33},
-      {:char=>"Z", :x_start=>219, :y_start=>14, :x_end=>236, :y_end=>33},
-      {:char=>"Z", :x_start=>255, :y_start=>14, :x_end=>269, :y_end=>33}
-    ])
-
-
+      {:char=>"X", :x_start=>222, :y_start=>14, :x_end=>238, :y_end=>32},
+      {:char=>"F", :x_start=>260, :y_start=>14, :x_end=>273, :y_end=>32}]
+    )
   end
 end
