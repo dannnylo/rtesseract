a8e49be3a0224508402b39654771880aeb11189f
Danilo Jeremias da Silva <daniloj.dasilva@gmail.com>
2018-01-05 00:23:57 -0200

    Update Gemfiles

--- spec/rtesseract_spec.rb
+++ spec/rtesseract_spec.rb
@@ -1,7 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 # encoding: UTF-8
 require 'pathname'
-
+require 'RMagick'
 # Class to rise error
 class MakeStringError
   def to_s
